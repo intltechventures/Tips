5b2ceccf21d1948a3799a824a007c33e6d48f998
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-05-02 11:03:23 -0700

    Added link to David Walsh, re: ReplaceSpacesInFileNames.sh

--- Scripts/Shell/ReplaceSpacesInFileNames.sh
+++ Scripts/Shell/ReplaceSpacesInFileNames.sh
@@ -1,4 +1,5 @@
 # tip of the hat to https://davidwalsh.name/remove-spaces-file-names
+# © David Walsh 2007-2016. All code MIT license
 # Replace spaces in file names with "_"
 for f in *\ *; do mv "$f" "${f// /_}"; done
 
